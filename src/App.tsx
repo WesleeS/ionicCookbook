8acab014f6b55fb97f7b346a95180c04526deede
WesleeS <ws160964@umconnect.umt.edu>
2024-03-25 09:37:09 -0600

    Solved an inf loop produced when querying the json data from the favorites in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Menu from './components/Menu';
 import Page from './pages/Page';
+import Search from './pages/Search';
+import Recipe from './pages/Recipe';
+import Region from './pages/Region';
+import Catagory from './pages/Catagory';
+import Favorited from './pages/Favorited'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,10 +38,29 @@ const App: React.FC = () => {
           <Menu />
           <IonRouterOutlet id="main">
             <Route path="/" exact={true}>
-              <Redirect to="/folder/Inbox" />
+              <Redirect to="/recipe/search" />
+            </Route>
+            <Route path="/recipe/search/region/:idRegion" exact={true}>
+              <Region />
+            </Route>
+            <Route path="/recipe/search/catagory/:idCatagory" exact={true}>
+              <Catagory />
+            </Route>
+            <Route path="/recipe/Favorites">
+              <Favorited />
             </Route>
             <Route path="/folder/:name" exact={true}>
               <Page />
+            </Route>
+            <Route path="/recipe/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/recipe/search/:query" exact={true}>
+              <Search />
+            </Route>
+            
+            <Route path="/recipe/view/:idMeal" exact={true}>
+              <Recipe />
             </Route>
           </IonRouterOutlet>
         </IonSplitPane>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,11 +8,16 @@ import {
   IonMenu,
   IonMenuToggle,
   IonNote,
+  IonAccordion,
+  IonAccordionGroup,
+  IonRouterLink,
 } from '@ionic/react';
 
 import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import { archiveOutline, archiveSharp, bookOutline, bookSharp, bookmarkOutline, bookmarkSharp, bookmarksSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, searchCircle, searchCircleSharp, searchOutline, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
 import './Menu.css';
+
+import React, { useEffect, useState } from 'react';
 
 interface AppPage {
   url: string;
@@ -23,54 +28,57 @@ interface AppPage {
 
 const appPages: AppPage[] = [
   {
-    title: 'Inbox',
-    url: '/folder/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/folder/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
+    title: 'Search',
+    url: '/recipe/search',
+    iosIcon: bookOutline,
+    mdIcon: bookSharp
   },
   {
     title: 'Favorites',
-    url: '/folder/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/folder/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/folder/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/folder/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
+    url: '/recipe/Favorites',
+    iosIcon: bookmarkOutline,
+    mdIcon: bookmarkSharp
   }
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
 const Menu: React.FC = () => {
   const location = useLocation();
+
+
+  const [regions, setRegions] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchLabels = async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
+      const data = await response.json();
+      setRegions(data.meals || []);
+    };
+
+    fetchLabels();
+  }, []);
+
+  const [catagories, setCatagories] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchLabels = async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`);
+      const data = await response.json();
+      setCatagories(data.meals || []);
+    };
+
+    fetchLabels();
+  }, []);
+
+  
+
 
   return (
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
+          <IonListHeader>Weslee's Cookbook of Pilfered Recipes</IonListHeader>
+          <IonNote>Referencing TheMealDB</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
@@ -84,13 +92,39 @@ const Menu: React.FC = () => {
         </IonList>
 
         <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
+          <IonAccordionGroup>
+          <IonAccordion value="Region">
+            <IonItem slot="header" color="light">
+              <IonLabel>Search by Region</IonLabel>
             </IonItem>
-          ))}
+            <div className="ion-padding" slot="content">
+            {regions.map((label, index) => (
+              <IonItem lines="none" key={index} routerLink={`/recipe/search/region/${label.strArea}`} 
+              className={location.pathname === `/recipe/search/region/${label.strArea}` ? 'selected' : ''}>
+                <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
+                <IonLabel>{label.strArea}</IonLabel>
+              </IonItem>
+            ))}
+            </div>
+          </IonAccordion>
+
+          <IonAccordion value="Catagory">
+            <IonItem slot="header" color="light">
+              <IonLabel>Search by Catagory</IonLabel>
+            </IonItem>
+            <div className="ion-padding" slot="content">
+            {catagories.map((label, index) => (
+              <IonItem lines="none" key={index} routerLink={`/recipe/search/catagory/${label.strCategory}`} 
+              className={location.pathname === `/recipe/search/catagory/${label.strCategory}` ? 'selected' : ''}>
+                <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
+                <IonLabel>{label.strCategory}</IonLabel>
+              </IonItem>
+            ))}
+            </div>
+          </IonAccordion>
+          </IonAccordionGroup>
+          
+
         </IonList>
       </IonContent>
     </IonMenu>
